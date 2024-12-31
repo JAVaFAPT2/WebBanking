d6ebe3247d1c5b28e80af376df30d7aa192028c3
Dang <dang.nh.909@aptechlearning.edu.vn>
2024-12-31 19:39:02 +0700

    updateDoc(Link)

---
+++ WB_BE/src/main/java/demo/wb_be/WbBeApplication.java
@@ -0,0 +1,13 @@
+package demo.wb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbBeApplication.class, args);
+	}
+
+}
