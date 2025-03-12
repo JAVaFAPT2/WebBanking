8882310007a6ae728bcbaf7c886ee3f07adeb024
Dang <dang.nh.909@aptechlearning.edu.vn>
2025-03-12 14:50:24 +0700

    itg update

--- API Gateway/src/main/java/org/api/gateway/ApiGatewayApplication.java
+++ API Gateway/src/main/java/org/api/gateway/ApiGatewayApplication.java
@@ -2,9 +2,11 @@ package org.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
+@EnableEurekaServer
 
 public class ApiGatewayApplication {
 
