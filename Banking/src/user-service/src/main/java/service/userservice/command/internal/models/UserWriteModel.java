5ccaf0aa218a1a2eead7d4973ac5e472fad7dec3
Dang <dang.nh.909@aptechlearning.edu.vn>
2025-04-01 20:20:37 +0700

    fix base line need to change base Project

--- Banking/src/user-service/src/main/java/service/userservice/command/internal/models/UserWriteModel.java
+++ Banking/src/user-service/src/main/java/service/userservice/command/internal/models/UserWriteModel.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.axonframework.commandhandling.model.AggregateRoot;
 import service.shared.models.Account;
 import service.shared.models.BaseEntity;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 import java.util.UUID;
 
 @Entity
+@AggregateRoot
 @Getter
 @Setter
 @AllArgsConstructor
